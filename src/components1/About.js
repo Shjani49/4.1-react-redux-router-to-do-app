fe96f108cde3f7a2264b6d71fababe062693b28d
Shjani49 <shjani49@gmail.com>
2020-07-29 14:26:09 -0600

    Add routes and navigation to the React webpage.

---
+++ src/components1/About.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class About extends React.Component{
+
+    render()
+    {
+        return (
+            <>
+            
+            <p>
+                Welcome to our React/Redux/Router To-Do App Example ! To learn more, Click 
+                &nbsp;
+                <a 
+                    href = "https://reactrouter.com/web/guides/quick-start"
+                    rel = "noopener noreferrer"
+                    target ="_blank">
+                    here for Router info!
+
+                </a>
+            </p>
+            </>
+
+        )
+    }
+}
+export default About;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+//import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 import './index.css';
 import { addNewToDo } from './actions/todos';
 import toDoReducer from './reducers/todos';
+import Nav from './components1/Nav';
 import App from './components1/App';
+import About from './components1/About';
 import { createStore } from 'redux';
 import {  Provider } from 'react-redux';
-
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { render } from '@testing-library/react';
 
 /**
  * Redux Store
@@ -27,13 +31,24 @@ store.subscribe( () => console.log( store.getState() ) ); // Outputs each time a
 store.dispatch( addNewToDo( "Buy milk." ) );
 store.dispatch( addNewToDo( "Practice React." ) );
 store.dispatch( addNewToDo( "Practice Redux." ) );
+// Set up a "root" for our Router.
+const Root = ( store ) => (
+  <Provider store = {store.store}>
+    <Router>
+    <h1> About React/Redux/Router To-Do Application</h1>
+      <Nav />
+      <Route path = "/" component= {App} exact/>
+      <Route path = "/about" component = {About} />
 
+         </Router>
+  </Provider>
 
-
-ReactDOM.render(
-    <Provider store = {store}>
-    <App />
-    </Provider>,
-  
-  document.getElementById('root')
+);
+Root.propTypes ={
+  store: PropTypes.object.isRequired
+}
+// Render the route-enabled configuration.
+render( 
+  <Root store ={store} />, 
+  document.getElementById( 'root' )
 );
